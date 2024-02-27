016d8e4bed1ac2188670784d4970ca262f5a24ad
MylesMburu <102133266+MylesMburu@users.noreply.github.com>
2024-02-27 21:48:34 +0300

    Update tips api

--- app/api/tips/route.js
+++ app/api/tips/route.js
@@ -2,9 +2,8 @@ import { connectToDB } from '@utils/database';
 import Tip from '@models/Tips';
 
 
-export const GET = async (request) => {
+export async function GET (request) {
   
-
   try {
     await connectToDB()
     const tips = await Tip.find({}) // fetch all tips
