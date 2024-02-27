56760c94122b550c560023c7208da5abac19648f
MylesMburu <102133266+MylesMburu@users.noreply.github.com>
2024-02-27 21:30:37 +0300

    get tips from db

--- app/api/tips/route.js
+++ app/api/tips/route.js
@@ -1,3 +1,15 @@
 import { connectToDB } from '@utils/database';
 import Tip from '@models/Tips';
 
+
+export const GET = async (request) => {
+  
+
+  try {
+    await dbConnect()
+    const tips = await Tip.find({}) // fetch all tips
+    return new Response(JSON.stringify(tips),{status: 200})
+  } catch (error) {
+    return new Response("Failed to get tips",{status: 200})
+  }
+}
