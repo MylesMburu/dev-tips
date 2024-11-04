daa9bb93f0c47620b28c2938e5c9e69cc815d80b
MylesMburu <ngicumyles@gmail.com>
2024-11-04 10:41:33 +0300

    Update tips api to prevent caching of responses

--- app/api/tips/route.js
+++ app/api/tips/route.js
@@ -7,6 +7,12 @@ export async function GET (request) {
   try {
     await connectToDB()
     const tips = await Tip.find({}) // fetch all tips
+
+    // Create a new Response object with headers to prevent caching
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    
     return new Response(JSON.stringify(tips),{status: 200})
   } catch (error) {
     return new Response("Failed to get tips",{status: 200})
